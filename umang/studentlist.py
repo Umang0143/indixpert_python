3becafd205f746e5e4469fa1c1b5138e2c04da49
Umang0143 <umangrajput1995@gmail.com>
2025-03-20 14:42:59 +0530

    studentlist

---
+++ umang/studentlist.py
@@ -0,0 +1,62 @@
+maindata=[]
+data={}
+skils=[]
+qualificationdict={}
+listdata=[]
+
+data["student id"]=int(input("please anter your id:-"))
+data["studentname"]=input("please anter your name:-")
+data["experience"]=input("Enter your experience:-")
+
+skils1=input("please enter your skils:-")
+skils.append(skils1)
+
+skils2=input("please enter your skils:-")
+skils.append(skils2)
+
+skils3=input("please enter your skils:-")
+skils.append(skils3)
+
+data["skils"]=skils
+
+qualificationdict["name"]=input("please enter your qualificatino:-")
+qualificationdict["pasingyear"]=int(input("please enter your pasing year:-"))
+listdata.append(qualificationdict)
+
+qualificationdict={}
+qualificationdict["name"]=input("please enter your qualificatino:-")
+qualificationdict["pasingyear"]=int(input("please enter your pasing year:-"))
+listdata.append(qualificationdict)
+
+data["qualification"]=listdata
+maindata.append(data)
+
+data={}
+skils=[]
+qualificationdict={}
+listdata=[]
+
+data["student id"]=int(input("please anter your id:-"))
+data["studentname"]=input("please anter your name:-")
+data["experience"]=input("Enter your experience:-")
+
+skils1=input("please enter your skils:-")
+skils.append(skils1)
+
+skils2=input("please enter your skils:-")
+skils.append(skils2)
+
+skils3=input("please enter your skils:-")
+skils.append(skils3)
+
+data["skils"]=skils
+
+qualificationdict["name"]=input("please enter your qualificatino:-")
+qualificationdict["pasingyear"]=int(input("please enter your pasing year:-"))
+listdata.append(qualificationdict)
+
+data["qualification"]=listdata
+maindata.append(data)
+
+
+print(maindata)
