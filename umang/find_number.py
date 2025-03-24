cdcbf8278575ff977af80835d540c3c44707d086
Umang0143 <umangrajput1995@gmail.com>
2025-03-24 23:41:14 +0530

    find number

---
+++ umang/find_number.py
@@ -0,0 +1,58 @@
+datalist=[1,2,3,4,5,6,7,8,9,10]
+
+smallnumber=[]
+largenumber=[]
+
+if datalist[0] < 5:
+    smallnumber.append(datalist[0])
+else:
+    largenumber.append(datalist[0])
+
+if datalist[1] < 5:
+    smallnumber.append(datalist[1])
+else:
+    largenumber.append(datalist[1])
+
+if datalist[2] < 5:
+    smallnumber.append(datalist[2])
+else:
+    largenumber.append(datalist[2])
+
+if datalist[3] < 5:
+    smallnumber.append(datalist[3])
+else:
+    largenumber.append(datalist[3])
+
+if datalist[4] < 5:
+    smallnumber.append(datalist[4])
+else:
+    largenumber.append(datalist[4])
+
+if datalist[5] < 5:
+    smallnumber.append(datalist[5])
+else:
+    largenumber.append(datalist[5])
+
+if datalist[6] < 5:
+    smallnumber.append(datalist[6])
+else:
+    largenumber.append(datalist[6])
+
+if datalist[7] < 5:
+    smallnumber.append(datalist[7])
+else:
+    largenumber.append(datalist[7])
+
+if datalist[8] < 5:
+    smallnumber.append(datalist[8])
+else:
+    largenumber.append(datalist[8])
+
+if datalist[9] < 5:
+    smallnumber.append(datalist[9])
+else:
+    largenumber.append(datalist[9])
+
+print("less than 5:-",smallnumber)
+
+print("greter than 5:-",largenumber)
