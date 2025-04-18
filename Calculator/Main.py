f2a56d6b59c943b157deb9fbb70a3591fecd1a6d
Umang0143 <umangrajput1995@gmail.com>
2025-04-18 14:45:03 +0530

    Main Function

---
+++ Calculator/Main.py
@@ -0,0 +1,14 @@
+import Option
+# from task.studente import sample
+
+def menu():
+    while(True):
+        print("==============Choice Number==============")
+        print("Press 1. Addition")
+        print("Press 2. Subtraction")
+        print("Press 3. Multiplication")
+        print("Press 4. Division")
+        print("Press 0. Exit")
+        print("=" * 40)
+        Option.choise()
+menu()
