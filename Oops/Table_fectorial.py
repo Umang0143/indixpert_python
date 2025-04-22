bd9f5d9bfd1d2357ed0795336838dfa1cc9d43cb
Umang0143 <umangrajput1995@gmail.com>
2025-04-23 00:04:30 +0530

    second task for table and fectorial

---
+++ Oops/Table_fectorial.py
@@ -0,0 +1,20 @@
+class Table:
+    
+    def __init__(self):
+        pass
+    
+    def get_table(self):
+        number=int(input("Enter your number:- "))
+        for n in range(1,11):
+            print(f"{number} * {n} = {number*n}")
+    
+    def factorial(self):
+        num=int(input("Enter your number:- "))
+        fact=num
+        for n in range(num-1,1,-1):
+            num *=n
+        print(f"factoril {fact} = {num}")
+
+data=Table()
+data.get_table()
+data.factorial()
